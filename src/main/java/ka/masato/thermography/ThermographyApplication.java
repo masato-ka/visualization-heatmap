a08fb98369cc8ac918cfe9953497935c59b1cebf
masato-ka <nsb21708@nifty.com>
2018-01-02 13:01:07 +0900

    Initial commit.

---
+++ src/main/java/ka/masato/thermography/ThermographyApplication.java
@@ -0,0 +1,12 @@
+package ka.masato.thermography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThermographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThermographyApplication.class, args);
+	}
+}
---
+++ src/test/java/ka/masato/thermography/ThermographyApplicationTests.java
@@ -0,0 +1,16 @@
+package ka.masato.thermography;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThermographyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
